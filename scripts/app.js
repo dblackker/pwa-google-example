79ccf93dcabd9562bb12856b4043226a320ec04e
Daniel Black <daniel.black@carsforsale.com>
2017-05-30 08:20:30 -0500

    Add push notification support.

--- scripts/app.js
+++ scripts/app.js
@@ -362,10 +362,126 @@
         app.saveSelectedCities();
     }
 
-    // Server worker code!
+    // Push Notifications
+    let pushButton = document.getElementById('butPush')
+    let pushRegistration;
+    let isSubscribed;
+
+    const applicationServerPublicKey = "BFtZcXPRMLyeoitOyfjPq3rKZn9MtjWzn3RWACNUvKbGJfcCQq-ZN7t7LkKL5j_BEhcQdlulpPLfyDq6td_ckrU";
+
+    // Service worker code!
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./service-worker.js')
-            .then(function() { console.log('Service Worker Registered'); });
+            .then(function (swReg) {
+                if ('PushManager' in window) {
+                    pushRegistration = swReg;
+                    initialiseUI();
+                }
+                console.log('Service Worker Registered');
+            });
+    }
+
+    function initialiseUI() {
+        pushButton.addEventListener('click', function() {
+            pushButton.disabled = true;
+            if (isSubscribed) {
+                unsubscribeUser();
+            } else {
+                subscribeUser();
+            }
+        });
+
+        // Set the initial subscription value
+        pushRegistration.pushManager.getSubscription()
+            .then(function(subscription) {
+                isSubscribed = !(subscription === null);
+
+                if (isSubscribed) {
+                    console.log('User IS subscribed.');
+                } else {
+                    console.log('User is NOT subscribed.');
+                }
+
+                updateBtn();
+            });
+    }
+
+    function updateBtn() {
+        if (Notification.permission === 'denied') {
+            pushButton.textContent = 'Push Messaging Blocked.';
+            pushButton.disabled = true;
+            updateSubscriptionOnServer(null);
+            return;
+        }
+
+        if (isSubscribed) {
+            pushButton.textContent = 'Disable Push Messaging';
+        } else {
+            pushButton.textContent = 'Enable Push Messaging';
+        }
+
+        pushButton.disabled = false;
+    }
+
+    function subscribeUser() {
+        const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+        pushRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: applicationServerKey
+        })
+            .then(function(subscription) {
+                console.log('User is subscribed.');
+
+                updateSubscriptionOnServer(subscription);
+
+                isSubscribed = true;
+
+                updateBtn();
+            })
+            .catch(function(err) {
+                console.log('Failed to subscribe the user: ', err);
+                updateBtn();
+            });
+    }
+
+    function unsubscribeUser() {
+        pushRegistration.pushManager.getSubscription()
+            .then(function(subscription) {
+                if (subscription) {
+                    return subscription.unsubscribe();
+                }
+            })
+            .catch(function(error) {
+                console.log('Error unsubscribing', error);
+            })
+            .then(function() {
+                updateSubscriptionOnServer(null);
+
+                console.log('User is unsubscribed.');
+                isSubscribed = false;
+
+                updateBtn();
+            });
+    }
+
+    function updateSubscriptionOnServer(subscription) {
+        // TODO: Send subscription to application server
+        console.log(JSON.stringify(subscription));
+    }
+
+    function urlB64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
     }
 })();
--- service-worker.js
+++ service-worker.js
@@ -80,3 +80,27 @@ self.addEventListener('fetch', function (e) {
         );
     }
 });
+
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'The weather is horrible today!',
+        icon: `/${projectDir}images/icons/icon-144x144.png`,
+        badge: `/${projectDir}images/icons/icon-144x144.png`
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
